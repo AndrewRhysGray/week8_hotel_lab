2aa21b4a230c732ed8d13f230019a76000fb7b93
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-01-22 13:21:09 +0000

    created basic form structure

---
+++ client/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Hotel Management</h1>
+    <bookings-form/>
+    <bookings-list/>
+  </div>
+</template>
+
+<script>
+import BookingsForm from './components/BookingsForm'
+import BookingsList from './components/BookingsList'
+
+export default {
+  name: 'app',
+  components: {
+    'bookings-form': BookingsForm,
+    'bookings-list': BookingsList
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ client/src/components/BookingsForm.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+  <form id="bookings-form" v-on:submit="handleFormSubmit">
+    <h3>Add a booking:</h3>
+
+    <label for="full-name">Full Name: </label>
+    <input type="text" id="full-name" >
+
+    <label for="email">Email: </label>
+    <input type="email" id="email" >
+
+    <label for="booked-in">Booking Status: </label>
+    <select id="booked-in">
+      <option value="true">Currently Booked</option>
+      <option value="false">Not Currently Booked</option>
+    </select>
+
+    <input type="submit" id="save" value="Save">
+  </form>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
