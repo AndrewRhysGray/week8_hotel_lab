5eff401f9d788331761cdb3d1e38d05f36c7a5b6
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-01-22 12:43:50 +0000

    most of crud working in insomnia

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -17,9 +17,42 @@ const createRouter = function (collection) {
     })
   })
 
+  router.get('/:id', (req, res) => {
+    id = req.params.id;
+    collection
+    .findOne({ _id: ObjectID(id)  })
+    .then((doc) => res.json(doc))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err })
+    })
+  })
+
+router.post('/', (req, res) => {
+  const newData = req.body
+  collection
+  .insertOne(newData)
+  .then (result => res.json(result.ops[0]))
+  .catch((err) => {
+    console.error(err);
+    res.status(500);
+    res.json({ status: 500, error: err })
+  })
+  })
 
 
-
+  router.delete('/:id', (req, res) => {
+    id = req.params.id;
+    collection
+    .deleteOne({ _id: ObjectID(id) })
+    .then((result) => res.json(result.result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err })
+    })
+  })
 
 
 
