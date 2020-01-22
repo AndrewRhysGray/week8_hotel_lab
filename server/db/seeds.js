17a79b1bce50f1dc387b012fe24113c81142b148
Andrew Gray <Andrew.rhysgray@hotmail.co.uk>
2020-01-22 11:17:13 +0000

    still in setup

---
+++ server/db/seeds.js
@@ -0,0 +1,21 @@
+use hotel;
+db.dropDataBase();
+
+db.bookings.insertMany([
+  {
+    guest_name: "Stephen Graham",
+    email_address: "SG@hotmail.co.uk",
+    checkin_status: true
+  },
+  {
+    guest_name: "Andrew Gray",
+    email_address: "Ag@hotmail.co.uk",
+    checkin_status: false
+    {
+      guest_name: "Cody Abbot",
+      email_address: "CD@hotmail.co.uk",
+      checkin_status: true
+    }
+
+  }
+]);
